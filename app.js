137d6813b583a004294b2b1558f3a0d51e3ff961
s533575 <s533575@nwmissouri.edu>
2018-10-26 22:08:57 -0500

    Added Procfile.txt

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const logger = require("morgan")
 const bodyParser = require("body-parser") // simplifies access to request body
 let fs=require('fs')
-let port=8080
+const port=process.env.PORT||8080
 const app = express()  // make express app
 const http = require('http').Server(app)  // inject app into the server
 
