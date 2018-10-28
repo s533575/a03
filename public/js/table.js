6191d2d9673891fa03b9227ceffff3aaa9816a98
s533575 <s533575@nwmissouri.edu>
2018-10-28 15:58:35 -0500

    changes made

--- public/js/table.js
+++ public/js/table.js
@@ -1,8 +1,13 @@
 $("#totalTable").hide()
-var Table = {
-    launch: launch
-}
-
+var Table =  function(){
+    $(document).ready(function() {
+        //console.log("DOM fully loaded and parsed - adding event listeners");
+        //document.getElementById('btnLaunch').addEventListener('click', App.launch)
+        // Suresh Chandra Peddinti has changed the following line to use jQuery click()
+        $("#tableclick").click(function(){
+        Table.launch()
+        });
+    });
 function launch()
 {
    
@@ -17,30 +22,36 @@ function launch()
 function getTable(number1,tillValue)
 {
     var printTable = ""
+    if (typeof number1 !== 'number' || typeof tillValue !== 'number') {
+        throw Error('The given argument is not a number')
+    }
     if(number1>0 && number1<=100)
     {
         if(tillValue>=0 && tillValue<=20)
         {
         for(var i=0;i<=tillValue;i++)
         {
-         //$("#id1").html(number1)
-         //$("#id2").html(i)
-         //$("#id3").html(number1*i)
+            return 1
         printTable+="<tr><td>"+number1+"</td><td>*</td><td>"+i+"</td><td>=</td><td>"+number1*i+"</td></tr>"
         console.log(printTable)
-
+         
         }
         }
         else
         {
+            return 0
          alert("You need to enter the table value from 0 to 20 only")
+        
         }
     }
     else
     {
+        return 0
         alert("The numbers should be between 0 to 101")
+    
     }
 
     $("#printtable").append(printTable)
     $("#totalTable").show()
 }
+}();
